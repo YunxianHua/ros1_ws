4849eff9664504c645ea00b23637897afa0c46f2
fei.gao <fei.gao@coscene.io>
2023-11-08 14:08:36 +0800

    feat: add foxglove bridge

---
+++ src/foxglove_bridge/cmake-build-debug/foxglove_bridge_base/src/version.cpp
@@ -0,0 +1,9 @@
+#include <foxglove_bridge/foxglove_bridge.hpp>
+
+namespace foxglove {
+
+const char FOXGLOVE_BRIDGE_VERSION[] = "0.7.3";
+
+const char FOXGLOVE_BRIDGE_GIT_HASH[] = "e6edd7b-dirty";
+
+}  // namespace foxglove
